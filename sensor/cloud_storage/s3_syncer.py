25cf9bc3ff5540e0b98e6ab70bce9ce0c2f96dc1
Rasika Gulhane <gulhanerasika@gmail.com>
2024-02-23 17:01:24 -0500

    minute change

--- sensor/cloud_storage/s3_syncer.py
+++ sensor/cloud_storage/s3_syncer.py
@@ -4,7 +4,6 @@
 import os
 class S3Sync:
 
-
     def sync_folder_to_s3(self,folder,aws_bucket_url):
         command = f"aws s3 sync {folder} {aws_bucket_url} "
         os.system(command)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -103,15 +103,15 @@ class TrainPipeline:
 
     def sync_artifact_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_buket_url=aws_buket_url)
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise SensorException(e,sys)
             
     def sync_saved_model_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
-            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise SensorException(e,sys)
         
