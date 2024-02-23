330e7ce61c9ff560c2c03399d222b109026eb81a
Rasika Gulhane <gulhanerasika@gmail.com>
2024-02-23 16:27:20 -0500

    added s3 configuration

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -1,3 +1,4 @@
+# For purpose of authentication with Database and AWS cloud we used this credential keys
 MONGODB_URL_KEY = 'MONGO_DB_URL'
 AWS_ACESS_KEY_ID_ENV_KEY = 'AWS_ACESS_KEY_ID'
 AWS_SECRET_ACESS_KEY_ENV_KEY = 'AWS_SECRET_ACESS_KEY'
--- sensor/constant/s3_bucket.py
+++ sensor/constant/s3_bucket.py
@@ -1,4 +1,6 @@
-TRAINING_BUCKET_NAME = "sensor-model"
+#We are using AWS CLI for easy and fast access
+
+TRAINING_BUCKET_NAME = "sensor-training-model"
 PREDICTION_BUCKET_NAME = "sensor-predicted"
 
 
